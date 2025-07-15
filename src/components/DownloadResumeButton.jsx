38619088d8de9b611caab6c6462ebcd8eb46aa40
varunkm11 <varunkumarsingh818@gmail.com>
2025-07-15 18:42:03 +0530

    Update DownloadResumeButton.jsx

--- src/components/DownloadResumeButton.jsx
+++ src/components/DownloadResumeButton.jsx
@@ -13,7 +13,7 @@ const DownloadResumeButton = () => {
       setIsDownloading(false);
       
       // Open Google Drive link in new tab (users can preview and download from there)
-      const driveLink = 'https://drive.google.com/file/d/1OquHcs8-oplCihJfEeytwM2YJpGtpzh0/view?usp=sharing';
+      const driveLink = 'https://drive.google.com/file/d/1iTcJHPIoWXBl1lokELWLv5J9tfoMp4Ab/view?usp=sharing';
       window.open(driveLink, '_blank');
     }, 1000);
   };
