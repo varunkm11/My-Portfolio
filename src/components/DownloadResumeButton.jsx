a2997f7a7034180c02fc4cf0603cf0604500214f
varunkm11 <varunkumarsingh818@gmail.com>
2025-10-30 21:25:59 +0530

    Update resume download link in button component
    Changed the Google Drive URL in DownloadResumeButton.jsx to point to a new resume file. Users will now be directed to the updated document when clicking the download button.

--- src/components/DownloadResumeButton.jsx
+++ src/components/DownloadResumeButton.jsx
@@ -13,7 +13,7 @@ const DownloadResumeButton = () => {
       setIsDownloading(false);
       
       // Open Google Drive link in new tab (users can preview and download from there)
-      const driveLink = 'https://drive.google.com/file/d/11gAtsJtKuW3DRMut_Y0gx66-Lco41RY1/view?usp=sharing';
+      const driveLink = 'https://drive.google.com/file/d/117qphdg4Y5q8YpVJeJCMBXqquFI9ruqw/view?usp=sharing';
       window.open(driveLink, '_blank');
     }, 1000);
   };
